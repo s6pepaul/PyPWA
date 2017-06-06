6283641da012dea40ce1a214b659d1411d82cf61
cjbanks <c.j.banks73079@spartans.nsu.edu>
2017-06-06 15:15:44 -0400

    Adds method to create a list of current directory and compares to new directory list

--- GUIs/directoryGUI.py
+++ GUIs/directoryGUI.py
@@ -11,27 +11,26 @@ from PyQt5.QtGui import QIcon
 
 from PyQt5.QtCore import QDir, QObject, pyqtSignal, Qt
 
-from filecmp import dircmp
-
 
 class DirView(QTreeView):
-    path = '/home/cjbanks/projects'
-    dir_diff = dircmp(path, path)
 
     def __init__(self):
         super(DirView, self).__init__()
+        self.__old_files = None
         self.__initui()
 
     def __initui(self):
         self.setGeometry(300, 300, 600, 580)
         self.setWindowTitle('Home Directory View')
         self.setWindowIcon(QIcon('web.png'))
-        self.__definingtree()
+        self.__defining_tree()
         self.textbox = QTextEdit()
+        self.textbox.setReadOnly(True)
+
+        self.__old_files = self.__make_list_of_files()
 
         btn = QPushButton('Recently Added Files', self)
         btn.clicked.connect(self.__file_added_event)
-
         splitter1 = QSplitter(Qt.Horizontal)
         splitter1.addWidget(self.tree)
         splitter1.addWidget(self.textbox)
@@ -49,7 +48,7 @@ class DirView(QTreeView):
         self.model.setRootPath(QDir.homePath())
         return self.model
 
-    def __definingtree(self):
+    def __defining_tree(self):
         # define QTreeView widget as a QFileSystem model
         self.tree = QTreeView()
         self.tree.setModel(self.__creating_model())
@@ -59,14 +58,30 @@ class DirView(QTreeView):
         self.tree.setSortingEnabled(True)
         self.tree.setWindowTitle("Dir View")
 
+    def __make_list_of_files(self):
+        path1 = '/home/cjbanks/dir1'
+        orig_files = []
+        with os.scandir(path1) as it:
+            for entry in it:
+                if not entry.name.startswith('.') and entry.is_file():
+                    orig_files.append(entry.name)
+        return orig_files
+
     # this defines the signal
-    def __file_added_event(self, dir_diff):
-        for filename in dir_diff.common_files:
-            if not dir_diff.common_files:
-                print(filename)
+    def __file_added_event(self):
+        new_files = []
+
+        with os.scandir('/home/cjbanks/dir1') as it:
+            for entry in it:
+                if not entry.name.startswith('.') and entry.is_file():
+                    new_files.append(entry.name)
+
+        for filename in new_files:
+            if filename not in self.__old_files:
                 self.textbox.textCursor().insertText(filename+'\n')
-        for sub_dir in dir_diff.subdirs.values():
-            self.__file_added_event(sub_dir)
+
+        if new_files == self.__old_files:
+            self.textbox.textCursor().insertText('No New Files \n')
 
 
 if __name__ == '__main__':
