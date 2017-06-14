5bb87bb3afb2ab3aa3451c8706cfa607a65095de
cjbanks <c.j.banks73079@spartans.nsu.edu>
2017-06-14 13:53:28 -0400

    Adds logging implementation
    Takes input from logger and displays to text box widget. This input
    should be file changes only.

--- GUIs/directoryGUI.py
+++ GUIs/directoryGUI.py
@@ -26,34 +26,29 @@ based on a button press.
 - _TreeInitialization - Creates a tree of the directory specified by main
 window.
 
--_CreateLogOfFiles - Creates a log that finds out whether files exist in it
-or not. Makes an entire log of added, deleted, and modified files
+-_TextBoxHandler - sends all log output from console to QTextEdit Widget
 
 -_DirectoryWatcher - Checks current directory for created, deleted or modified
 files. Returns any changes to a text box.
-
--_UpdateUserOnNewFileEntry - Initializes _DirectoryWater to create a initial
-object containing directory data. Once the directory changes, a trigger is
-activated and the updates are displayed in a directory
 
 -_Splitter - Creates a widget that vertically splits a display of a
 directory tree(_TreeInitialization) and a text box with file changes
 (_UpdateUserOnNewFileEntry).
 
--_DirectoryView - Places splitter widget in a layout. Main object to create
-this widget
+-_DirectoryView - Places splitter widget in a layout and initializes
+ files observer object. Main object to create
+this widget.
 
 """
 
+import logging
 import sys
-import watchdog
 
-from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtGui
-
-from watchdog import utils
-from watchdog.utils import dirsnapshot
+from PyQt5 import QtWidgets
+from watchdog import events
+from watchdog import observers
 
 
 class _TreeInitialization(QtWidgets.QTreeView):
@@ -76,201 +71,65 @@ class _TreeInitialization(QtWidgets.QTreeView):
         self.setWindowTitle("Dir View")
 
 
-class _CreateLogOfFiles(object):
+class _TextBoxHandler(logging.Handler):
     def __init__(self):
-        super(_CreateLogOfFiles, self).__init__()
-        self.__added_files_log = []
-        self.__deleted_files_log = []
-        self.__modified_files_log = []
+        super(_TextBoxHandler, self).__init__()
+        self.text_box = QtWidgets.QTextEdit()
+        self.text_box.setReadOnly(True)
+        self.text_box.textCursor().atEnd()
+        self.__initialize_text_box_handler()
 
-    def add_file_to_added_log(self, file):
-        if file not in self.__added_files_log:
-            self.__added_files_log.append(file)
+    def emit(self, record):
+        message = self.format(record)
+        self.text_box.insertPlainText(message)
 
-    def add_file_to_deleted_log(self, file):
-        if file not in self.__deleted_files_log:
-            self.__deleted_files_log.append(file)
-
-    def add_file_to_modified_log(self, file):
-        if file not in self.__modified_files_log:
-            self.__modified_files_log.append(file)
-
-    def get_files_from_added_log(self, list_of_added_files):
-        # self.__take_snapshot_of_current_directory()
-        for file in self.__added_files_log:
-            if file not in list_of_added_files:
-                list_of_added_files.append(file)
-        list_of_added_files.insert(0, 'File(s) added: \n')
-        return list_of_added_files
-
-    def get_files_from_deleted_log(self, list_of_deleted_files):
-        # self.__take_snapshot_of_current_directory()
-        for file in self.__deleted_files_log:
-            if file not in list_of_deleted_files:
-                list_of_deleted_files.append(file)
-        list_of_deleted_files.insert(0, 'File(s) removed: \n')
-        return list_of_deleted_files
-
-    def get_files_from_modified_log(self, list_of_modified_files):
-        # removed take snapshot
-        for file in self.__modified_files_log:
-            if file not in list_of_modified_files:
-                list_of_modified_files.append(file)
-        list_of_modified_files.insert(0, 'File(s) Modified: \n')
-        return list_of_modified_files
-
-
-class _DirectoryWatcher(object):
-    def __init__(self, initial_path):
-        super(_DirectoryWatcher, self).__init__()
-        self.__initial_path = initial_path
-        self.__current_directory = None
-        self.__updated_directory = None
-        self.compare_directories = None
-        self.number_of_files_before = 0
-        self.number_of_files_after = 0
-
-    def take_snapshot_of_current_directory(self):
-        self.__current_directory = watchdog.utils.dirsnapshot. \
-            DirectorySnapshot(self.__initial_path, recursive=False)
-        self.number_of_files_before = len(self.__current_directory.paths)
-        print('Length of old Files: ', len(self.__current_directory.paths))
-        return self.__current_directory
-
-    def take_snapshot_of_updated_directory(self):
-        self.__updated_directory = watchdog.utils.dirsnapshot. \
-            DirectorySnapshot(self.__initial_path, recursive=False)
-        self.number_of_files_after = len(self.__updated_directory.paths)
-        print('Length of updated list: ', len(self.__updated_directory.paths))
-        return self.__updated_directory
-
-    def compare_changes_in_directories(self):
-        print(self.__updated_directory.paths)
-        self.compare_directories = watchdog.utils.dirsnapshot. \
-            DirectorySnapshotDiff(self.__current_directory,
-                                  self.__updated_directory)
-        return self.compare_directories
-
-
-class _GetChanged(object):
-
-    def __init__(self, initial_path, directory_watcher):
-        # type: (str, _DirectoryWatcher) -> None
-        super(_GetChanged, self).__init__()
-        self.__initial_path = initial_path
-        self.list_of_added_files = []
-        self.list_of_deleted_files = []
-        self.list_of_modified_files = []
-        self.directory_watcher_elements = directory_watcher
-        self.__stored_snapshot = directory_watcher.\
-            compare_changes_in_directories()
-        self.__get_list_of_files()
-        self.__compare_lists_to_logs()
-
-    def __get_list_of_files(self):
-        self.list_of_added_files = self.__stored_snapshot.files_created
-        self.list_of_deleted_files = self.__stored_snapshot.files_deleted
-        self.list_of_modified_files = self.__stored_snapshot.files_modified
-        print('Added : ', self.list_of_added_files)
-        print(self.list_of_deleted_files)
-        print(self.list_of_deleted_files)
-
-    def __compare_lists_to_logs(self):
-        for file in self.list_of_added_files:
-            _CreateLogOfFiles().add_file_to_added_log(file)
-
-        for file in self.list_of_deleted_files:
-            _CreateLogOfFiles().add_file_to_deleted_log(file)
-
-        for file in self.list_of_modified_files:
-            _CreateLogOfFiles().add_file_to_modified_log(file)
-
-    def return_log_of_files(self):
-        number_of_files_before = self.directory_watcher_elements.\
-            number_of_files_before
-        number_of_files_after = self.directory_watcher_elements.\
-            number_of_files_after
-        print('After: ', number_of_files_after)
-        print('Before: ', number_of_files_before)
-
-        if number_of_files_after > number_of_files_before:
-            old_list = self.list_of_added_files
-            updated_added_files_list \
-                = _CreateLogOfFiles().get_files_from_added_log(old_list)
-            return updated_added_files_list
-
-        elif number_of_files_after < number_of_files_before:
-            old_list = self.list_of_deleted_files
-            updated_deleted_files_list \
-                = _CreateLogOfFiles().get_files_from_deleted_log(old_list)
-            return updated_deleted_files_list
-
-        elif len(self.list_of_modified_files) > 0:
-            old_list = self.list_of_modified_files
-            updated_modified_files_list \
-                = _CreateLogOfFiles().get_files_from_modified_log(old_list)
-            return updated_modified_files_list
-
-        else:
-            listeria = ['No New Files \n']
-            return listeria
-
-
-class _UpdateUserOnNewFileEntry(QtWidgets.QTextEdit):
-    def __init__(self, initial_path):
-        super(_UpdateUserOnNewFileEntry, self).__init__()
-        self.setReadOnly(True)
-        self.__directory_updater = _DirectoryWatcher(initial_path)
-        self.__directory_updater.take_snapshot_of_current_directory()
-        self.__initial_path = initial_path
-        self.__trigger(initial_path)
-
-    def __trigger(self, initial_path):
-        self.__new_file_trigger = QtCore.QFileSystemWatcher(self)
-        self.__new_file_trigger.addPath(initial_path)
-        self.__new_file_trigger.directoryChanged.connect(
-            self.__update_file_display)
-
-    def __update_file_display(self):
-        self.clear()
-        self.__directory_updater.take_snapshot_of_updated_directory()
-        updates_display = _GetChanged(self.__initial_path,
-                                      self.__directory_updater)
-        changed_list = updates_display.return_log_of_files()
-        print('something happened')
-        for file in changed_list:
-            self.textCursor().insertText(file + '\n')
-        # _DirectoryWatcher(self.__initial_path)
+    def __initialize_text_box_handler(self):
+        self.setFormatter(logging.Formatter(
+            '%(message)s '))
+        logging.getLogger().addHandler(self)
+        logging.getLogger().setLevel(logging.INFO)
 
 
 class _Splitter(QtWidgets.QSplitter):
     def __init__(self, initial_path):
         super(_Splitter, self).__init__()
         self.addWidget(_TreeInitialization(initial_path))
-        self.addWidget(_UpdateUserOnNewFileEntry(initial_path))
+        __text_box_handle = _TextBoxHandler()
+        self.addWidget(__text_box_handle.text_box)
 
 
 class _DirectoryView(QtWidgets.QWidget):
     def __init__(self, initial_path):
         super(_DirectoryView, self).__init__()
+        self.__initialize_observer(initial_path)
         self.__initialize_ui(initial_path)
 
+    def __initialize_observer(self, initial_path):
+        __event_handler = events.LoggingEventHandler()
+        self.__observer = observers.Observer()
+        self.__observer.schedule(__event_handler,
+                                 initial_path, recursive=True)
+        self.__observer.start()
+
     def __initialize_ui(self, initial_path):
-        vertical_box = QtWidgets.QVBoxLayout()
-        vertical_box.addWidget(_Splitter(initial_path))
-        self.setLayout(vertical_box)
+        __vertical_box = QtWidgets.QVBoxLayout()
+        __vertical_box.addWidget(_Splitter(initial_path))
+        self.setLayout(__vertical_box)
 
 
 class _MainWindow(QtWidgets.QMainWindow):
+
+    __LOGGER = logging.getLogger(__name__ + "._MainWindow")
+
     def __init__(self):
         super(_MainWindow, self).__init__()
         self.setGeometry(300, 300, 600, 580)
         self.setWindowTitle('Home Directory View')
         self.setWindowIcon(QtGui.QIcon('web.png'))
-        self.setCentralWidget(_DirectoryView(QtCore.QDir.homePath()))
+        self.setCentralWidget(_DirectoryView(QtCore.QDir.currentPath()))
         self.show()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    directory_view = _MainWindow()
+    dir_view = _MainWindow()
     sys.exit(app.exec_())
