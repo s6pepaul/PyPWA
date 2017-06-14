3f8ec31e6d8206b58f9579440f2c27acd84126eb
cjbanks <c.j.banks73079@spartans.nsu.edu>
2017-06-14 13:53:28 -0400

    Adds logging implementation
    Takes input from logger and displays to text box widget. This input
    should be file changes only.

--- GUIs/test_directoryGUI.py
+++ GUIs/test_directoryGUI.py
@@ -1,20 +1,46 @@
+import os
+import PyQt5
 import pytest
 import sys
-import os
+import logging
 
-sys.path.append(".")
 
-from GUIs.directoryGUI import _DirectoryWatcher
+from PyQt5 import QtCore
+from GUIs import directoryGUI
+from testfixtures import LogCapture
+
+@pytest.fixture()
+def main_window():
+    return directoryGUI._MainWindow()
+
+
+def test_main_window_opens(qtbot, main_window):
+    qtbot.addWidget(main_window)
+    qtbot.waitExposed(main_window, timeout=1000)
 
 
 @pytest.fixture()
-def file_watcher():
-    return _DirectoryWatcher(sys.path[-1])
+def directory_view():
+    return directoryGUI._DirectoryView('.')
 
 
-def test_detects_file_change(file_watcher):
-    open("testfile", "w").close()
-    files = file_watcher.get_changed_files()
-    assert "testfile" in files
-    os.remove("testfile")
-    assert "testfile" in files
+def test_directory_view(qtbot, directory_view):
+    qtbot.addWidget(directory_view)
+    qtbot.waitExposed(directory_view, timeout=1000)
+
+
+@pytest.fixture()
+def tree_viewer():
+    return directoryGUI._TreeInitialization('.')
+
+
+def test_tree_opens(qtbot, tree_viewer):
+    qtbot.addWidget(tree_viewer)
+    qtbot.waitExposed(tree_viewer, timeout=1000)
+
+
+@pytest.fixture()
+def text_box():
+    return directoryGUI._TextBoxHandler()
+
+
