8d6080221beaac8b98d88b28c0782a464b958ca9
cjbanks <c.j.banks73079@spartans.nsu.edu>
2017-06-15 15:28:29 -0400

    Implements class for file change handling
    File change class that is used by an observer thread from watchdog API
    updates a QWidget object to display changes to a directory and file.

--- GUIs/directoryGUI.py
+++ GUIs/directoryGUI.py
@@ -26,10 +26,8 @@ based on a button press.
 - _TreeInitialization - Creates a tree of the directory specified by main
 window.
 
--_TextBoxHandler - sends all log output from console to QTextEdit Widget
-
--_DirectoryWatcher - Checks current directory for created, deleted or modified
-files. Returns any changes to a text box.
+-_FileChangeHandler - is accessed by observer thread, once a file is
+modified/created/deleted, text box is updated.
 
 -_Splitter - Creates a widget that vertically splits a display of a
 directory tree(_TreeInitialization) and a text box with file changes
@@ -41,9 +39,7 @@ this widget.
 
 """
 
-import logging
 import sys
-
 from PyQt5 import QtCore
 from PyQt5 import QtGui
 from PyQt5 import QtWidgets
@@ -52,15 +48,15 @@ from watchdog import observers
 
 
 class _TreeInitialization(QtWidgets.QTreeView):
-    def __init__(self, initial_path):
+    def __init__(self, __initial_path):
         super(_TreeInitialization, self).__init__()
         self.__model = None
-        self.__creating_model(initial_path)
-        self.__defining_tree(initial_path)
+        self.__creating_model(__initial_path)
+        self.__defining_tree(__initial_path)
 
-    def __creating_model(self, initial_path):
+    def __creating_model(self, __initial_path):
         self.__model = QtWidgets.QFileSystemModel()
-        self.__model.setRootPath(initial_path)
+        self.__model.setRootPath(__initial_path)
 
     def __defining_tree(self, initial_path):
         self.setModel(self.__model)
@@ -71,56 +67,73 @@ class _TreeInitialization(QtWidgets.QTreeView):
         self.setWindowTitle("Dir View")
 
 
-class _TextBoxHandler(logging.Handler):
+class _FileChangeHandler(events.FileSystemEventHandler):
+
     def __init__(self):
-        super(_TextBoxHandler, self).__init__()
+        super(_FileChangeHandler, self).__init__()
         self.text_box = QtWidgets.QTextEdit()
         self.text_box.setReadOnly(True)
-        self.text_box.textCursor().atEnd()
-        self.__initialize_text_box_handler()
+        self.__text = str()
 
-    def emit(self, record):
-        message = self.format(record)
-        self.text_box.insertPlainText(message)
+    def on_moved(self, __event):
+        __what = 'directory' if __event.is_directory else 'file'
+        self.__text = 'Moved {0}: from {1} to {2} \n'.format(
+            __what, __event.src_path, __event.dest_path
+        )
+        self.text_box.insertPlainText(self.__text)
 
-    def __initialize_text_box_handler(self):
-        self.setFormatter(logging.Formatter(
-            '%(message)s '))
-        logging.getLogger().addHandler(self)
-        logging.getLogger().setLevel(logging.INFO)
+    def on_created(self, __event):
+        __what = 'directory' if __event.is_directory else 'file'
+        self.__text = 'Created {0}: {1} \n'.format(__what, __event.src_path)
+        self.text_box.insertPlainText(self.__text)
+
+    def on_deleted(self, __event):
+        __what = 'directory' if __event.is_directory else 'file'
+        self.__text = 'Deleted {0}: {1} \n'.format(__what, __event.src_path)
+        self.text_box.insertPlainText(self.__text)
+
+    def on_modified(self, __event):
+        __what = 'directory' if __event.is_directory else 'file'
+        self.__text = 'Modified {0}: {1} \n'.format(__what, __event.src_path)
+        self.text_box.insertPlainText(self.__text)
 
 
 class _Splitter(QtWidgets.QSplitter):
-    def __init__(self, initial_path):
+
+    def __init__(self, initial_path, __event_handler):
         super(_Splitter, self).__init__()
         self.addWidget(_TreeInitialization(initial_path))
-        __text_box_handle = _TextBoxHandler()
-        self.addWidget(__text_box_handle.text_box)
+        self.addWidget(__event_handler.text_box)
 
 
 class _DirectoryView(QtWidgets.QWidget):
-    def __init__(self, initial_path):
-        super(_DirectoryView, self).__init__()
-        self.__initialize_observer(initial_path)
-        self.__initialize_ui(initial_path)
 
-    def __initialize_observer(self, initial_path):
-        __event_handler = events.LoggingEventHandler()
+    def __init__(self, __initial_path):
+        super(_DirectoryView, self).__init__()
+        self.__initial_path = __initial_path
+        self.__event_handler = _FileChangeHandler()
+        self.__initialize_observer()
+        self.__initialize_ui()
+
+    def __initialize_observer(self):
         self.__observer = observers.Observer()
-        self.__observer.schedule(__event_handler,
-                                 initial_path, recursive=True)
+        self.__observer.schedule(
+            self.__event_handler, self.__initial_path, recursive=True
+        )
         self.__observer.start()
 
-    def __initialize_ui(self, initial_path):
+    def __initialize_ui(self):
         __vertical_box = QtWidgets.QVBoxLayout()
-        __vertical_box.addWidget(_Splitter(initial_path))
+        __vertical_box.addWidget(_Splitter(
+            self.__initial_path, self.__event_handler
+        ))
         self.setLayout(__vertical_box)
+
+    def __del__(self):
+        self.__observer.stop()
 
 
 class _MainWindow(QtWidgets.QMainWindow):
-
-    __LOGGER = logging.getLogger(__name__ + "._MainWindow")
-
     def __init__(self):
         super(_MainWindow, self).__init__()
         self.setGeometry(300, 300, 600, 580)
@@ -128,6 +141,7 @@ class _MainWindow(QtWidgets.QMainWindow):
         self.setWindowIcon(QtGui.QIcon('web.png'))
         self.setCentralWidget(_DirectoryView(QtCore.QDir.currentPath()))
         self.show()
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
